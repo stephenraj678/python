615124e5356ff5c407e6685140cf7b647d4cf395
stephenraj678 <53745612+stephenraj678@users.noreply.github.com>
2019-08-08 01:56:50 -0700

    Update py13.py

--- py13.py
+++ py13.py
@@ -1,12 +1,14 @@
 #S
 import sys
 n = int(input())
-if n==1 : print('no')
-elif n==2 or n==3 : print('yes')
-else :
-    for i in range(2,n) :
-	if n%i == 0 :
-	    print('no')
-	    sys.exit()
-    else :
+if n==1:
+	print('no')
+elif n==2 or n==3:
 	print('yes')
+else:
+	for i in range(2,n):
+		if n%i==0:
+			print('no')
+			sys.exit()
+	else:
+		print('yes')
